a5eea49043d79be8bfaf2e9c178b1e9419d7f751
Sid Joshi <imsj.226@hotmail.com>
2015-07-22 23:35:01 -0300

    successfull youtube api call using google api client library

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,17 @@
 'use strict';
 
 (function($){
-	
+	function load(){
+		gapi.client.setApiKey('AIzaSyBEZBDf9MFGDBHF1jq1lGC0dpUocPSrdow');
+		gapi.client.load('youtube', 'v3').then(function(){console.log('loaded')});
+	}
+
+	$.getScript('js/gapi.min.js')
+	.done(function(e){
+		$.getScript('js/gapi.loaded.min.js')
+		.done(function(e){load()})
+		.fail(function(e){console.log("Failed - " + e)});
+	})
+	.fail(function(e){console.log(e.error)});
+
 })(jQuery)
